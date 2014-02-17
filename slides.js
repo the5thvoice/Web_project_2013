b53d82d53d7a17e4139bc9706e70378f4284d70e
thomas byrne <thomas.jo.byrne@gmail.com>
2014-02-17 17:41:04 +0000

    changes

---
+++ slides.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  				  $('.slides').cycle({
+					fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+					speed: 2500,
+					pause: 1
+				});
+			});
