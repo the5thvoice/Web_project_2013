00fc68b583fe99a05a350a44de42f14d875db61c
thomas byrne <thomas.jo.byrne@gmail.com>
2014-02-17 17:43:48 +0000

    Revert "changes"
    This reverts commit b53d82d53d7a17e4139bc9706e70378f4284d70e.

--- slides.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-  				  $('.slides').cycle({
-					fx: 'fade', // choose your transition type, ex: fade, scrollUp, shuffle, etc...
-					speed: 2500,
-					pause: 1
-				});
-			});
